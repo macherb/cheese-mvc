7e9381f554d52667417244b9c1c798c35d3a08ae
Bobby Macher <bobby_macher@hotmail.com>
2017-12-20 09:59:09 -0600

    Replaced cheeseId and nextId with id Changed CheeseType to Category

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,12 +1,19 @@
-package com.example.cheesemvc.models;
+package org.launchcode.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 /**
  * Created by LaunchCode
  */
+@Entity
 public class Cheese {
+
+    @Id
+    @GeneratedValue
+    private int id;
 
     @NotNull
     @Size(min=3, max=15)
@@ -16,28 +23,28 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    @ManyToOne
+    private Category category;
 
-    private int cheeseId;
-    private static int nextId = 1;
+    ///@ManyToMany(mappedBy = "cheeses")
+    ///private List<Menu> menus;
+
+    ///private List<Cheese> cheeses;
 
     public Cheese(String name, String description) {
-        this();
         this.name = name;
         this.description = description;
     }
 
     public Cheese() {
-        cheeseId = nextId;
-        nextId++;
     }
 
-    public int getCheeseId() {
-        return cheeseId;
+    public int getId() {
+        return id;
     }
 
-    public void setCheeseId(int cheeseId) {
-        this.cheeseId = cheeseId;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -56,11 +63,19 @@ public class Cheese {
         this.description = description;
     }
 
-    public CheeseType getType() {
-        return type;
+    public Category getCategory() {
+        return category;
     }
 
-    public void setType(CheeseType type) {
-        this.type = type;
+    public void setCategory(Category category) {
+        this.category = category;
     }
+
+    /*public List<Menu> getMenus() {
+        return menus;
+    }
+
+    public void setMenus(List<Menu> menus) {
+        this.menus = menus;
+    }*/
 }
