d778af71b1c887ea385f899ce8f41201566f4804
Bobby Macher <bobby_macher@hotmail.com>
2017-12-31 02:16:09 -0600

    Import Errors. verify doesn't need to be added in add's GET. user must be valid, remove verify, and errors is required in add's POST. Moved saving of userDao to bottom of function. Added return "user/add" if hasErrors, at top of function. Removed return "user/add" if verify is null. Don't add verify before returning "user/add". Return "redirect:" in all other cases.

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.launchcode.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,45 +28,51 @@ public class UserController {
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model) {
-        String  verify = "password";
+//        String  verify = "password";
         model.addAttribute("title", "Add User");
         model.addAttribute(new User());
-        model.addAttribute("verify", "verify");
-        model.addAttribute(verify);
+//        model.addAttribute("verify", "verify");
+//        model.addAttribute(verify);
         return "user/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @ModelAttribute User user, @ModelAttribute @Valid String verify) {
-        userDao.save(user);
+    public String add(Model model, @ModelAttribute @Valid User user, /*@ModelAttribute @Valid String verify, */Errors errors) {
+        if (errors.hasErrors()) {
+//            model.addAttribute("title", "other error");
+//            model.addAttribute("verify", "verify");
+            return "user/add";
+        }
+
         //model.addAttribute(user);
-        if (verify == null) {
+/*        if (verify == null) {
             model.addAttribute("title", "verify is null");
             model.addAttribute("verify", "verify");
             return "user/add";
         }
         //model.addAttribute(verify);
-        if (user == null) {
+*/        if (user == null) {
             model.addAttribute("title", "user is null");
-            model.addAttribute("verify", "verify");
+//            model.addAttribute("verify", "verify");
             return "user/add";
         }
         else if (user.getPassword() == null) {
             model.addAttribute("title", "password is null");
-            model.addAttribute("verify", "verify");
+//            model.addAttribute("verify", "verify");
             return "user/add";
         }
         else if (user.getPassword().equals("")) {
             model.addAttribute("title", "password is empty");
-            model.addAttribute("verify", "verify");
+//            model.addAttribute("verify", "verify");
             return "user/add";
         }
         else if (!user.getPassword().equals(user.getVerify())) {
             model.addAttribute("title", "password \"" + user.getPassword() + "\" not equal to verify \"" + user.getVerify() + '"');
-            model.addAttribute("verify", "verify");
+//            model.addAttribute("verify", "verify");
             return "user/add";
         }
         model.addAttribute("title", "Hello " + user.getUsername());
-        return "user/index";//"redirect:";
+        userDao.save(user);
+        return "redirect:";//"user/index";//
     }
 }
