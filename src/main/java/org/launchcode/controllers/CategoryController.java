42162787156cb3bed105decc7808f697e8b8a7d1
Bobby Macher <bobby_macher@hotmail.com>
2018-01-07 04:01:14 -0600

    Use static variables instead of literal strings. Added Get's and Set's remove and edit. remove's Post only displays which cheese belong to that category.

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,14 +1,14 @@
 package org.launchcode.controllers;
 
 import org.launchcode.models.Category;
+import org.launchcode.models.Cheese;
 import org.launchcode.models.data.CategoryDao;
+import org.launchcode.models.data.CheeseDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -18,11 +18,14 @@ public class CategoryController {
     @Autowired
     private CategoryDao categoryDao;
 
+    @Autowired
+    private CheeseDao cheeseDao;
+
     @RequestMapping(value = "")
     public String index(Model model) {
 
         model.addAttribute("categories", categoryDao.findAll());
-        model.addAttribute("title", "Categories");
+        model.addAttribute("title", Category.titleList);
 
         return "category/index";
     }
@@ -40,10 +43,62 @@ public class CategoryController {
     public String add(Model model, @ModelAttribute @Valid Category category, Errors errors) {
 
         if (errors.hasErrors()) {
-            model.addAttribute("title", "Add Cheese");
+            model.addAttribute("title", Category.titleAdd);
             return "category/add";
         }
         categoryDao.save(category);
         return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCategoryForm(Model model) {
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("title", Category.titleRemove);
+        return "category/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCategoryForm(Model model, @RequestParam int[] categoryIds) {
+        int i = 0;
+        String all="";
+        final Iterable<Cheese> cheeses=cheeseDao.findAll();
+        for (int categoryId: categoryIds) {
+            final Category category = categoryDao.findOne(categoryId);
+            for (Cheese cheese : cheeses) {
+                if (cheese.getCategory() == category) {
+                    i++;
+                    all += " " + cheese.getName();
+                }
+                else {
+                    ;//categoryDao.delete(category);
+                }
+            }
+        }
+        model.addAttribute("categories", categoryDao.findAll());
+        model.addAttribute("title", i + " cheese(s) found:" + all);
+        return "category/remove";//"redirect:";
+    }
+
+    @RequestMapping(value = "edit/{categoryId}", method = RequestMethod.GET)
+    public String displayEditForm(Model model, @PathVariable int categoryId) {
+        model.addAttribute("title", "Edit Category");
+        Category newCategory = categoryDao.findOne(categoryId);
+        model.addAttribute("categoryId", categoryId);
+        model.addAttribute("name", newCategory.getName());
+
+        return "category/edit";
+    }
+
+    @RequestMapping(value = "edit/{categoryId}", method = RequestMethod.POST)
+    public String processEditForm(Model model,
+                                  @PathVariable int categoryId,
+                                  String name) {
+        Category newCategory = categoryDao.findOne(categoryId);
+
+        newCategory.setName(name);
+
+        categoryDao.save(newCategory);
+        model.addAttribute("categories", categoryDao.findAll());
+        return "category/index";//return "redirect:";//return "category/edit";
     }
 }
